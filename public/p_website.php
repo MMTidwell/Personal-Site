de0ad8669e1975b549d97a90e24ddcf47c0ebc70
Mittsy Tidwell <mtidwell506@gmail.com>
2016-08-23 09:44:31 -0500

    Navbar looking good

---
+++ public/p_website.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>mtidwell.tech</title>
+
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="/bootstrap-3.3.6-dist/css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="/p_website.css">
+	</head>
+
+	<body>
+		<!-- NAVBAR -->
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+		    <div class="container">
+
+		        <!-- Brand and toggle get grouped for better mobile display -->
+		        <!-- This is the dropdown menu when the browser is small (creates the hamburger) -->
+		        <div class="navbar-header">
+		            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		            </button>
+		            <a class="navbar-brand" href="#">Mittsy Tidwell</a>
+		        </div>
+
+		        <!-- Links in profile -->
+		        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		            <ul class="nav navbar-nav navbar-right">
+		                <li><a href="#">About</a></li>
+		                <li><a href="#">Code</a></li>
+		                <li><a href="#">Connect</a></li>
+		                <li><a href="#">Blog</a></li>
+		                <li><a href="#">Contact</a></li>
+		            </ul>
+
+		        </div><!-- /.navbar-collapse -->
+		    </div><!-- /.container-fluid -->
+		</nav>
+
+
+
+   
+
+
+	<!-- Bootstrap core JavaScript -->
+	<!-- ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+
+	<!-- AJAX and JS
+	================================================== -->
+	<!-- bootstarp JS -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<!-- JS -->
+	<script src="/p_website.js"></script>
+
+	</body>
+</html>
