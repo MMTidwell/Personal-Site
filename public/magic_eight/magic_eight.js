1f777849c66ce18250ef8253a5c8b70b3722737c
Mittsy Tidwell <mtidwell506@gmail.com>
2016-09-19 18:41:14 -0500

    got everything working :)

--- public/magic_eight/magic_eight.js
+++ public/magic_eight/magic_eight.js
@@ -24,74 +24,14 @@
 
 	// ====================== RANDOM NUM GEN =============================
 	// ====================== ANSWER =============================
-	function randomNumber() { 
-		var num = Math.floor(Math.random() * 20) + 1;
-		// console.log("your number is: " + num)
+	var answers = ['Maybe', 'Sure', 'Certainly not', 'I hope for your sake', 'Nope!', 'There is a possibility', 'Quite likely', 'I hope not!', 'NEVER!!', 'Ahaha! Really?!?', 'Pfft', 'Hell YES!', 'Hell to the NO', 'Hell YES!!', ' Who cares?', 'Yes', 'It is certain', 'It is decidedlu so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
 
-		setTimeout(function() {
-			switch (num) {
-				case 1:
-					$(".triangle").html("It is certain");
-					break;
-				case 2:
-					$(".triangle").html("It is decidedly so");
-					break;
-				case 3:
-					$(".triangle").html("Without a doubt");
-					break;
-				case 4:
-					$(".triangle").html("Yes, definitely");
-					break;
-				case 5:
-					$(".triangle").html("You may rely on it");
-					break;
-				case 6:
-					$(".triangle").html("As I see it, yes");
-					break;
-				case 7:
-					$(".triangle").html("Most likely");
-					break;
-				case 8:
-					$(".triangle").html("Outlook good");
-					break;
-				case 9:
-					$(".triangle").html("Yes");
-					break;
-				case 10:
-					$(".triangle").html("Signs point to yes");
-					break;
-				case 11:
-					$(".triangle").html("Reply hazy try again");
-					break;
-				case 12:
-					$(".triangle").html("Ask again later");
-					break;
-				case 13:
-					$(".triangle").html("Better not tell you now");
-					break;
-				case 14:
-					$(".triangle").html("Cannot predict now");
-					break;
-				case 15:
-					$(".triangle").html("Concentrate and ask again");
-					break;
-				case 16:
-					$(".triangle").html("Don't count on it");
-					break;
-				case 17:
-					$(".triangle").html("My reply is no");
-					break;
-				case 18:
-					$(".triangle").html("My sources say no");
-					break;
-				case 19:
-					$(".triangle").html("Outlook not so good");
-					break;
-				case 20:
-					$(".triangle").html("Very doubtful");
-					break;
-			}
-		}, 700);
+	function randomNumber() { 
+		var random = Math.floor((Math.random() * answers.length - 1) + 0);
+		var answer = answers[random];
+			setTimeout(function() {
+				$(".triangle").html(answer);
+			}, 700);
 	}
 	
 play();
